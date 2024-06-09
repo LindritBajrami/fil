1f7149d2be744fd94bc22045fce8eb86330ac262
Lindrit Bajrami <145931723+LindritBajrami@users.noreply.github.com>
2024-06-09 23:57:06 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
     return `
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-          <a class="navbar-brand" href="index.html">FilmaDB</a>
+          <a class="navbar-brand" href="index.html">Movies Shop</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
