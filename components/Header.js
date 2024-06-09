26f4b619a5c0f61b080986384a0697c714f1e09c
Lindrit Bajrami <145931723+LindritBajrami@users.noreply.github.com>
2024-06-09 23:58:41 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
                 <a class="nav-link" href="movies.html">Movies</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="favourites.html">Favourite Movies</a>
+                <a class="nav-link" href="favourites.html">Favourite</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="contact.html">Contacts</a>
