2587c0815cc2d2ca71949440777cc5115bf82971
Lindrit Bajrami <145931723+LindritBajrami@users.noreply.github.com>
2024-06-10 00:07:13 +0200

    Update Movie.js

--- components/Movie.js
+++ components/Movie.js
@@ -24,8 +24,8 @@ export default function Movie(id, div) {
                     <br />
                     Votes: ${data.vote_count}
                 </p>
-                <button id="add-to-fav" movie-id="${data.id}" class="btn btn-sm btn-outline-danger"><i class="bi bi-heart"></i> Add to favourite movies</button>
-                <a href="movies.html" class="btn btn-sm btn-outline-secondary ms-2"><i class="bi bi-arrow-left"></i> Return to Movies</a>
+                <button id="add-to-fav" movie-id="${data.id}" class="btn btn-sm btn-outline-danger"><i class="bi bi-heart"></i> Add to favorites</button>
+                <a href="movies.html" class="btn btn-sm btn-outline-secondary ms-2"><i class="bi bi-arrow-left"></i> Return back</a>
             </div>
         </div>
         `
